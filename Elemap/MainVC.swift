6c41b682f5a9a0033c861800fb341674f83cfa8c
Igor Karyi <igorkaryi@ukr.net>
2018-01-14 00:02:57 +0200

    LaunchScreen

---
+++ Elemap/MainVC.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Elemap
+//
+//  Created by Igor Karyi on 13.01.2018.
+//  Copyright © 2018 Igor Karyi. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
+
+
+}
+
